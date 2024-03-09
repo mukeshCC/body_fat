dc142fd38ca8edb9b4b5ab4e46da4eb6b4759be4
mukeshCC <127389240+mukeshCC@users.noreply.github.com>
2024-03-09 12:34:35 +0530

    Update bf_web.py

--- bf_web.py
+++ bf_web.py
@@ -83,7 +83,11 @@ if st.button('Predict Body Fat Percentage'):
 
 # Display the uploaded image
 st.subheader('Body_Fat_Description')
-st.image('body_desc_img.png', use_column_width=True)
+import os
+image_path = os.path.abspath('body_desc_img.png')
+st.image(image_path, caption='Body Description Image', use_column_width=True)
+
+# st.image('body_desc_img.png', use_column_width=True)
 
 
 
